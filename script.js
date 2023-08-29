bbb92dc4614b28d3c0280259e702a7cb3d4b6223
Adam <martia123456789@gmail.com>
2023-08-29 16:50:48 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const groceries = []
+import {LocalDB} from 'https://cdn.skypack.dev/peadb';
+import shortid from 'https://cdn.skypack.dev/shortid'
+
+const db = new LocalDB('grocery-list-db')
+const groceries = db.getAll() || []
 
 const groceryList = document.getElementById('groceryList')
 const newGroceryInput = document.getElementById('newGrocery')
 const addBtn = document.getElementById('addBtn')
 
-const createGroceryElement = groceryName => {
+const createGroceryElement = grocery => {
     const groceryElement = document.createElement('li')
-    groceryElement.innerText = groceryName
+    groceryElement.innerText = grocery.value
     groceryElement.classList.add('groceryItem')
     return groceryElement
 }
@@ -19,8 +23,11 @@ addBtn.addEventListener('click', e => {
     e.preventDefault()
 
     const value = newGroceryInput.value
+    
     if (value) {
-        addGrocery(value)
+        const key = shortid.generate()
+        addGrocery({key, value})
+        db.set(key, value)
         newGroceryInput.value = null
     }
 })
